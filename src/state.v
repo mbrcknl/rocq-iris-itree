a8cec5cfc77ecf1829d14ff002fd6d6765076941
Max Vistrup <contact@maxv.dk>
2023-06-12 22:53:12 +0200

    Prove basic lemmata for `WPi` for `stateH`.

--- src/state.v
+++ src/state.v
@@ -3,6 +3,8 @@ From iris.base_logic.lib Require Import iprop.
 From iris.base_logic.lib Require Import ghost_var.
 From iris.base_logic.lib Require Export fancy_updates.
 From iris.itree Require Import handler.
+From iris.itree Require Import wpi.
+From ITree Require Import ITree.
 
 Class stateHPreG (Σ : gFunctors) (S : Type) := StateHPreG {
   stateH_pre_ghost_varG :> ghost_varG Σ S;
@@ -36,7 +38,8 @@ Arguments EFork {_}.
 (** State interpretation predicate which is enforced at every [EYield]. *)
 Class stateInterp (Σ : gFunctors) (S : Type) := state_interp : S → iProp Σ.
 
-(* TODO: Switch to some kind of authoritative thing. *)
+(* TODO: Switch to some authoritative thing. *)
+(** Asserts read-access to the state *)
 Definition state_is {Σ S} `{!stateHGS Σ S} (s : S) : iProp Σ :=
   ∃ q, ghost_var stateH_name q s.
 (** Asserts writable access to the state.
@@ -64,3 +67,36 @@ Definition forkH {Σ} (S : Type) `{!stateHGS Σ S} : iHandler Σ (stateE S) :=
 
 Definition stateH {Σ} (S : Type) `{!stateHGS Σ S} `{!stateInterp Σ S} `{!invGS_gen HasNoLc Σ} : iHandler Σ (stateE S) :=
   get_stateH S ∪ set_stateH S ∪ yieldH S ∪ forkH S.
+
+Section wp_state.
+  Context {S : Type} `{!stateHGS Σ S} {E : Type → Type} {H : iHandler Σ E}.
+  Context `{!stateInterp Σ S} `{!invGS_gen HasNoLc Σ}.
+  Context `{stateE S -< E} `{inH Σ (stateE S) E (stateH S) H}.
+
+  Lemma wp_get {R} (s : S) (k : S → itree E R) (Φ : R → iProp Σ) :
+    state_is s -∗
+    ▷ WPi (k s) @ H {{ Φ }} -∗
+    WPi (vis EGetState k) @ H {{ Φ }}.
+  Proof.
+    iIntros "Hstate Hwp". iApply wpi_vis. iApply is_inH. iNext.
+    do 3 iLeft. iExists eq_refl. iExists s. eauto with iFrame.
+  Qed.
+
+  Lemma wp_set {R} (s s' : S) (k : unit → itree E R) (Φ : R → iProp Σ) :
+    state_own s -∗
+    ▷ (state_own s' -∗ WPi (k tt) @ H {{ Φ }}) -∗
+    WPi (vis (ESetState s') k) @ H {{ Φ }}.
+  Proof.
+    iIntros "Hown Hwp". iApply wpi_vis. iApply is_inH. iNext.
+    do 2 iLeft. iRight. iExists eq_refl. iExists s, s'.
+    eauto with iFrame.
+  Qed.
+
+  Lemma wp_fork {R} (k : bool → itree E R) (Φ : R → iProp Σ) :
+    (▷ WPi (k true) @ H {{ Φ }} ∗ ▷ WPi (k false) @ H {{ Φ }}) -∗
+    WPi (vis EFork k) @ H {{ Φ }}.
+  Proof.
+    iIntros "[Hwp1 Hwp2]". iApply wpi_vis. iNext. iApply is_inH.
+    iRight. iExists eq_refl. iFrame.
+  Qed.
+End wp_state.
