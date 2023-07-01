e8ffcafa6e63224779ccf4637c85b01cededeaf2
Max Vistrup <contact@maxv.dk>
2023-07-01 08:55:31 +0200

    State interleaving theorem.

--- src/threadpool.v
+++ src/threadpool.v
@@ -5,6 +5,11 @@ From iris.base_logic.lib Require Export fancy_updates.
 From iris.itree Require Import handler.
 From iris.itree Require Import wpi.
 From ITree Require Import ITree.
+From ITree Require Import Eq.
+From stdpp Require Import list.
+From Paco Require Import paco.
+From Paco Require Import paco3.
+Import EqNotations.
 
 (** An event type transformer for adding concurrency. *)
 CoInductive threadpoolE (E : Type → Type) : Type → Type :=
@@ -41,7 +46,7 @@ Next Obligation.
 Qed.
 
 Section wp_threadpool.
-  Context {S : Type} `{!invGS_gen HasNoLc Σ} {E : Type → Type} {H : iHandler Σ E}.
+  Context `{!invGS_gen HasNoLc Σ} {E : Type → Type} {H : iHandler Σ E}.
 
   Lemma wpi_fork {R} (t : itree (threadpoolE E) unit) (k : unit → itree (threadpoolE E) R) (M : coPset) (Φ : R → iProp Σ) :
     (▷ WPi (k tt) @ threadpoolH H; M {{ Φ }} ∗ ▷ WPi t @ threadpoolH H; ⊤ {{ _, True }}) -∗
@@ -67,3 +72,77 @@ Section wp_threadpool.
     iNext. rewrite -wpi_clear_mask. iMod "Hfupd". by iMod "Hwp".
   Qed.
 End wp_threadpool.
+
+Section interleaving.
+  Context `{!invGS_gen HasNoLc Σ} {E : Type → Type} {H : iHandler Σ E}.
+
+  Variant interleavesF
+    (interleaves : list (itree (threadpoolE E) unit) -> itree (threadpoolE E) unit -> itree E unit -> Prop)
+    :  list (itree (threadpoolE E) unit)
+    -> itree' (threadpoolE E) unit
+    -> itree' E unit
+    -> Prop :=
+  | Terminates :
+    interleavesF interleaves [] (RetF tt) (RetF tt)
+  | ThreadEnds tp new_current interleaving' :
+    new_current < length tp →
+    interleaves (delete new_current tp) (nth new_current tp (Ret tt)) interleaving' →
+    interleavesF interleaves tp (RetF tt) (TauF interleaving')
+  | Steps current' tp interleaving' :
+    interleavesF interleaves tp (TauF current') (TauF interleaving')
+  | Emits tp A e k k' :
+    (∀ a, interleaves tp (k' a) (k a)) →
+    interleavesF interleaves tp (VisF (EEmit E A e) k') (VisF e k)
+  | Yields tp k new_current interleaving' :
+    new_current < length tp →
+    interleaves (cons (k tt) (delete new_current tp)) (nth new_current tp (Ret tt)) interleaving' →
+    interleavesF interleaves tp (VisF EYield k) (TauF interleaving')
+  | Forks tp t k interleaving' :
+    interleaves (cons t tp) (k tt) interleaving' →
+    interleavesF interleaves tp (VisF (EFork t) k) (TauF interleaving').
+  Hint Constructors interleavesF : iris_itree.
+  Definition interleaves_
+    (interleaves : list (itree (threadpoolE E) unit) -> itree (threadpoolE E) unit -> itree E unit -> Prop)
+    :  list (itree (threadpoolE E) unit)
+    -> itree (threadpoolE E) unit
+    -> itree E unit
+    -> Prop :=
+    fun tp current interleaving =>
+    interleavesF interleaves tp (observe current) (observe interleaving).
+
+  Lemma interleavesF_mono interleaves interleaves' tp current interleaving :
+    interleaves <3= interleaves' →
+    interleavesF interleaves tp current interleaving →
+    interleavesF interleaves' tp current interleaving.
+  Proof.
+    intros Hleq HinterleavesF. destruct HinterleavesF; eauto with iris_itree.
+  Qed.
+  Lemma interleaves__mono :
+    monotone3 interleaves_.
+  Proof.
+    rewrite /monotone3 /interleaves_. intros. by eapply interleavesF_mono; last done.
+  Qed.
+  Hint Resolve interleaves__mono : paco.
+
+  Definition interleaves :
+    list (itree (threadpoolE E) unit) -> itree (threadpoolE E) unit -> itree E unit -> Prop :=
+    paco3 interleaves_ bot3.
+
+  Theorem interleaving
+    (tp : list (itree (threadpoolE E) unit))
+    (current : itree (threadpoolE E) unit)
+    (interleaving : itree E unit) :
+    interleaves tp current interleaving →
+    ([∗ list] thread ∈ tp, WPi thread @ threadpoolH H; ⊤ {{ _, True }}) -∗
+    WPi current @ threadpoolH H; ∅ {{ _, |={∅, ⊤}=> True }} -∗
+    WPi interleaving @ H; ⊤ {{ _, True }}.
+  Proof.
+    iIntros "%Hinter Htp Hcurrent".
+    punfold Hinter. induction Hinter.
+  Admitted.
+
+  Corollary interleaving_simple (concurrent : itree (threadpoolE E) unit) (interleaving : itree E unit) :
+    interleaves [] concurrent interleaving →
+    WPi concurrent @ threadpoolH H; ⊤ {{ _, True }} -∗
+    WPi interleaving @ H; ⊤ {{ _, True }}.
+End interleaving.
