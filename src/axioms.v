ddc6e05acdb888dcb733c0f4b31864c5e2b86f9e
Max Vistrup <contact@maxv.dk>
2023-08-11 11:27:17 +0200

    Add link Michael showed me to comments.

--- src/axioms.v
+++ src/axioms.v
@@ -2,12 +2,12 @@ From Coq.Logic Require Import ChoiceFacts EqdepFacts.
 From stdpp Require Import tactics.
 
 Module Ax : EqdepElimination.
-
-(* Invariance by Substitution of Reflexive Equality Proofs (already implied XM), UIP (`Eq_rect_eq` from `Coq.Logic.EqdepFacts`) *)
-Axiom eq_rect_eq :
-    forall (U:Type) (p:U) (Q:U -> Type) (x:Q p) (h:p = p),
+  (** Invariance by Substitution of Reflexive Equality Proofs. This is implied by
+  XM. Also, it is equivalent to UIP. See [1].
+  [1] https://github.com/coq/coq/wiki/The-Logic-of-Coq#what-axioms-can-be-safely-added-to-coq. *)
+  Axiom eq_rect_eq :
+    ∀ (U : Type) (p : U) (Q : U → Type) (x : Q p) (h : p = p),
       x = eq_rect p Q x p h.
-
 End Ax.
 
 Module Export UIPM := EqdepTheory Ax.
