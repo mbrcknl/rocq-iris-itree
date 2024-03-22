c4daeb414d02ab45ba2c2c043c6b517bd6cf9939
Max Vistrup <contact@maxv.dk>
2024-03-22 14:25:17 +0100

    Restructure handler and adequacy theorem for threadpools into separate files.

---
+++ src/threadpool/handler.v
@@ -0,0 +1,86 @@
+From iris.itree Require Import handler.
+From iris.itree Require Import wpi.
+From iris.proofmode Require Import proofmode.
+From iris.base_logic.lib Require Import iprop.
+From iris.base_logic.lib Require Export fancy_updates.
+From ITree Require Import ITree.
+
+(** Thread continuation. *)
+Variant thread :=
+ | CurrentThread
+ | NewThread.
+(** An event type for concurrency. *)
+Variant threadpoolE : Type → Type :=
+  (** Unix-style fork that splits the thread (copying its stack) into two
+  threads, distinguished by the answer value of type [[thread]]. Execution
+  continues in the thread with answer [CurrentThread] until a [EYield] is
+  emitted. *)
+  | EFork : threadpoolE thread
+  (** Yield control to another (demonically chosen) thread in the thread-pool. *)
+  | EYield : threadpoolE unit
+  (** (Safely) kill the current thread and yield. *)
+  | EKillThread : threadpoolE Empty_set.
+
+(** [iHandler] for [threadpoolE]. *)
+Program Definition threadpoolH {Σ} `{!invGS_gen hlc Σ} : iHandler Σ threadpoolE :=
+  IHandler (λ A e,
+    match e with
+    (** We define the iHandler in a way that imposes a semantic restriction on
+    [EFork] disallowing returning in the [NewThread] continuation, even
+    though it is possible to define [itree]s that do so. From the point of view
+    of [WPi], we are thus declaring such returns as unsafe. This
+    overapproximation is justified from our applications: typically, only the
+    return value of the main thread concerns us. *)
+    | EFork      => λ Φ s, Φ CurrentThread ∗ s NewThread
+    (** To prove that one can [EYield], one must restablish all the invariants. *)
+    | EYield     => λ Φ _, |={∅, ⊤}=> |={⊤, ∅}=> Φ tt
+    | EKillThread => λ _ _, |={∅, ⊤}=> True
+    end
+  )%I _.
+Next Obligation.
+  iIntros (?????????) "HΦwand #Hswand". destruct e.
+  - iIntros "[HΦ Hs]". iSplitL "HΦwand HΦ".
+    * by iApply "HΦwand".
+    * by iApply "Hswand".
+  - iIntros "HΦ". by iApply "HΦwand".
+  - by iIntros "?".
+Qed.
+
+(** Stepping lemmata for the threadpool [WPi]. *)
+Section wp_threadpool.
+  Context `{!invGS_gen hlc Σ} {E : Type → Type} {H : iHandler Σ E}.
+  Context `{threadpoolE -< E} `{inH Σ threadpoolE E threadpoolH H}.
+
+  Lemma wpi_fork {R} (k : thread → itree E R) (M : coPset) (Φ : R → iProp Σ) :
+    WPi (k CurrentThread) @ H; M {{ Φ }} ∗ WPi (k NewThread) @ H; ⊤ {{ _, False }} -∗
+    WPi (vis EFork k) @ H; M {{ Φ }}.
+  Proof.
+    iIntros "[Hwpcur Hwpnew]". iApply wpi_vis.
+    rewrite /threadpoolH. iFrame.
+    iApply fupd_mask_intro; first apply empty_subseteq. iIntros "Hfupd".
+    iApply is_inH. simpl. iSplitL "Hwpcur Hfupd".
+    - rewrite -wpi_clear_mask. iApply wpi_update. iMod "Hfupd". iMod "Hwpcur". by iModIntro.
+    - done.
+  Qed.
+
+  (** Note here crucially that the mask has to be full for the rule to apply.
+  This means that you cannot step over an [EYield] if there are open
+  invariants. It amounts to the typical requirement of atomicity in the
+  invariant opening rule known from "normal Iris". *)
+  Lemma wpi_yield (Φ : () → iProp Σ) :
+    Φ () -∗
+    WPi (trigger EYield) @ H; ⊤ {{ Φ }}.
+  Proof.
+    iIntros "HΦ". iApply wpi_vis. iApply is_inH. simpl.
+    iApply fupd_mask_intro_subseteq; first done.
+    iApply fupd_mask_intro; first apply empty_subseteq. iIntros "Hfupd".
+    iApply wpi_ret. by iMod "Hfupd".
+  Qed.
+
+  Lemma wpi_kill {R} (k : Empty_set → itree E R) (Φ : R → iProp Σ) :
+    ⊢ WPi (vis EKillThread k) @ H; ⊤ {{ Φ }}.
+  Proof.
+    iApply wpi_vis. iApply is_inH. simpl.
+    by iApply fupd_mask_intro_subseteq; first done.
+  Qed.
+End wp_threadpool.
