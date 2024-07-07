99c537a4f487d14dfa2bb334774bcaf32f41ff49
Max Vistrup <contact@maxv.dk>
2024-07-07 14:23:14 +0200

    Add support to array allocation in `heap.v`.

--- src/heap.v
+++ src/heap.v
@@ -9,7 +9,7 @@ From iris.proofmode Require Import proofmode.
 
 (** A heap, represented as a [gmap] of [option val]s, with [None] representing
 deallocated locations. *)
-Definition heap V : Type := (gmap loc (option V)).
+Notation heap V := (gmap loc (option V)).
 (** [HeapE V] is the event type for manipulating a heap. *)
 Definition heapE V : Type → Type := stateE (heap V).
 
@@ -96,23 +96,69 @@ Section free_locations.
   Qed.
 End free_locations.
 
-Definition alloc `{!heapE V -< E} (v : V) : itree E loc :=
+Fixpoint heap_array {V} (l : loc) (vs : list V) : heap V :=
+  match vs with
+  | [] => ∅
+  | v :: vs' => {[l := Some v]} ∪ heap_array (l +ₗ 1) vs'
+  end.
+
+Lemma heap_array_singleton {V} l (v : V) :
+    heap_array l [v] = {[l := Some v]}.
+Proof. by rewrite /heap_array right_id. Qed.
+
+Lemma heap_array_lookup {V} l vs (ow : option V) k :
+  heap_array l vs !! k = Some ow ↔
+  ∃ j w, (0 ≤ j)%Z ∧ k = l +ₗ j ∧ ow = Some w ∧ vs !! (Z.to_nat j) = Some w.
+Proof.
+  revert k l; induction vs as [|v' vs IH]=> l' l /=.
+  { rewrite lookup_empty. naive_solver lia. }
+  rewrite -insert_union_singleton_l lookup_insert_Some IH. split.
+  - intros [[-> ?] | (Hl & j & w & ? & -> & -> & ?)].
+    { eexists 0, _. rewrite Loc.add_0. naive_solver lia. }
+    eexists (1 + j)%Z, _. rewrite Loc.add_assoc !Z.add_1_l Z2Nat.inj_succ; auto with lia.
+  - intros (j & w & ? & -> & -> & Hil). destruct (decide (j = 0)); simplify_eq/=.
+    { rewrite Loc.add_0; eauto. }
+    right. split.
+    { rewrite -{1}(Loc.add_0 l). intros ?%(inj (Loc.add _)); lia. }
+    assert (Z.to_nat j = S (Z.to_nat (j - 1))) as Hj.
+    { rewrite -Z2Nat.inj_succ; last lia. f_equal; lia. }
+    rewrite Hj /= in Hil.
+    eexists (j - 1)%Z, _. rewrite Loc.add_assoc Z.add_sub_assoc Z.add_simpl_l.
+    auto with lia.
+Qed.
+
+Lemma heap_array_map_disjoint {V} (h : gmap loc (option V)) (l : loc) (vs : list V) :
+  (∀ i, (0 ≤ i)%Z → (i < length vs)%Z → h !! (l +ₗ i) = None) →
+  (heap_array l vs) ##ₘ h.
+Proof.
+  intros Hdisj. apply map_disjoint_spec=> l' v1 v2.
+  intros (j&w&?&->&?&Hj%lookup_lt_Some%inj_lt)%heap_array_lookup.
+  move: Hj. rewrite Z2Nat.id // => ?. by rewrite Hdisj.
+Qed.
+
+Definition allocN `{!heapE V -< E} (n : nat) (v : V) : itree E loc :=
   (* Read the entire heap. *)
   σ ← trigger EGetState;
   (* Deterministically pick a free location on the heap. *)
-  let l : free_locations 1 σ := inhabitant in
+  let l : free_locations n σ := inhabitant in
     (* Write the evaluated value [v] to every memory cell in that segment. *)
-    trigger (ESetState (<[`l:=Some v]> σ));;
+    trigger (ESetState ((heap_array (`l) (replicate (Z.to_nat n) v)) ∪ σ));;
     Ret (`l).
 
-Definition alloc_nondet `{!heapE V -< E} `{demonicE -< E} (v : V) : itree E loc :=
+Definition allocN_nondet `{!heapE V -< E} `{demonicE -< E} (n : nat) (v : V) : itree E loc :=
   (* Read the entire heap. *)
   σ ← trigger EGetState;
   (* Demonically pick a free location of the heap. *)
-  l ← trigger (EDemonic (free_locations 1 σ));
+  l ← trigger (EDemonic (free_locations n σ));
   (* Write the evaluated value [v] to every memory cell in that segment. *)
-  trigger (ESetState (<[`l:=Some v]> σ));;
+  trigger (ESetState ((heap_array (`l) (replicate (Z.to_nat n) v)) ∪ σ));;
   Ret (`l).
+
+Definition alloc `{!heapE V -< E} (v : V) : itree E loc :=
+  allocN 1 v.
+
+Definition alloc_nondet `{!heapE V -< E} `{demonicE -< E} (v : V) : itree E loc :=
+  allocN_nondet 1 v.
 
 Class heapHGpreS (Σ : gFunctors) (V : Type) := HeapHGpreS {
   heapH_ghost_varG :> ghost_mapG Σ loc (option V);
@@ -222,10 +268,27 @@ Section wp.
     by iApply "Hwand".
   Qed.
 
-  Lemma wpi_alloc M v Φ :
+  Lemma big_sep_map_list_heap_array l n m v :
+    ([∗ map] k↦v0 ∈ heap_array (l +ₗ Z.of_nat m) (replicate n v), k ↦? v0) -∗
+    [∗ list] i ∈ seq m n, (l +ₗ Z.of_nat i) ↦ v.
+  Proof.
+    iIntros "Hsep".
+    iInduction n as [|n'] "IH" forall (m).
+    - done.
+    - simpl.
+      iDestruct (big_sepM_union with "Hsep") as "[Hfirst Hsep]".
+      { symmetry. apply heap_array_map_disjoint. intros i Hnz Hlt. rewrite lookup_singleton_None.
+        rewrite Loc.eq_spec. simpl. lia. }
+      rewrite big_sepM_singleton. iFrame.
+      iApply "IH".
+      replace (l +ₗ S m) with (l +ₗ m +ₗ 1); last first. { rewrite Loc.add_assoc. f_equiv. lia. }
+      done.
+  Qed.
+
+  Lemma wpi_allocN M n v Φ :
     ↑heapH_inv_name ⊆ M →
-    (∀ l, l ↦ v -∗ Φ l) -∗
-    WPi alloc v @ H; M {{ Φ }}.
+    (∀ l, ([∗ list] i ∈ seq 0 n, (l +ₗ (i : nat)) ↦ v) -∗ Φ l) -∗
+    WPi allocN n v @ H; M {{ Φ }}.
   Proof.
     iIntros (Hmask) "Hwand". iApply wpi_clear_mask.
     iApply wpi_bind. iApply wpi_get.
@@ -238,16 +301,29 @@ Section wp.
     iApply wpi_ret. iApply wpi_bind. iApply @wpi_set. iIntros (σ'') "[Hauth' _]".
     iDestruct (ghost_map_auth_agree with "Hauth Hauth'") as %<-.
     iCombine "Hauth Hauth'" as "Hauth".
-    destruct (inhabitant : free_locations 1 σ) as [l Hfree].
+    destruct (inhabitant : free_locations n σ) as [l Hfree].
     simpl. apply bool_decide_unpack in Hfree.
-    iMod (ghost_map_insert l (Some v) with "Hauth") as "[Hauth Hpointsto]".
-    { replace l with (l +ₗ0) by apply Loc.add_0. by apply Hfree. }
+    iMod (ghost_map_insert_big (heap_array l (replicate n v)) with "Hauth") as "[Hauth Hpointsto]".
+    { apply heap_array_map_disjoint. destruct l as [l Hl]. intros i Hnz Hlt.
+      rewrite replicate_length in Hlt.
+      apply Hfree; first done. lia.
+    }
     iDestruct "Hauth" as "[Hauth Hauth']".
-    iFrame "Hinv". iFrame.
+    iFrame "Hinv".
+    replace (Z.to_nat n) with n by lia. iFrame.
     repeat iApply wpi_ret. iApply "Hwand". iModIntro. iMod "Hfupd".
     iMod ("Hclose" with "[Hauth]") as "_".
     { by iExists _. }
-    done.
+    iApply big_sep_map_list_heap_array. rewrite Loc.add_0 //.
+  Qed.
+
+  Lemma wpi_alloc M v Φ :
+    ↑heapH_inv_name ⊆ M →
+    (∀ l, l ↦ v -∗ Φ l) -∗
+    WPi alloc v @ H; M {{ Φ }}.
+  Proof.
+    iIntros (Hmask) "Hwand". iApply wpi_allocN; first done.
+    simpl. iIntros (l) "[Hpointsto _]". rewrite Loc.add_0. by iApply "Hwand".
   Qed.
 End wp.
 
@@ -256,10 +332,10 @@ Section wp_nondet.
   Context `{!invGS_gen hlc Σ} `{!heapHGS Σ V} `{inH Σ (heapE V) E (heapH V) H}.
   Context `{demonicE -< E} `{inH Σ demonicE E demonicH H}.
 
-  Lemma wpi_alloc_nondet M v Φ :
+  Lemma wpi_allocN_nondet M n v Φ :
     ↑heapH_inv_name ⊆ M →
-    (∀ l, l ↦ v -∗ Φ l) -∗
-    WPi alloc_nondet v @ H; M {{ Φ }}.
+    (∀ l, ([∗ list] i ∈ seq 0 n, (l +ₗ (i : nat)) ↦ v) -∗ Φ l) -∗
+    WPi allocN_nondet n v @ H; M {{ Φ }}.
   Proof.
     iIntros (Hmask) "Hwand". iApply wpi_clear_mask.
     iApply wpi_bind. iApply wpi_get.
@@ -270,17 +346,30 @@ Section wp_nondet.
     iFrame "Hauth' Hinv".
     iApply fupd_mask_intro; first apply empty_subseteq. iIntros "Hfupd".
     iApply wpi_ret. iApply wpi_bind. iApply @wpi_demonic. iIntros ([l Hfree]). iApply wpi_ret.
-    iApply wpi_bind. iApply @wpi_set. iIntros (σ'') "[Hauth' _]".
+     iApply wpi_bind. iApply @wpi_set. iIntros (σ'') "[Hauth' _]".
     iDestruct (ghost_map_auth_agree with "Hauth Hauth'") as %<-.
     iCombine "Hauth Hauth'" as "Hauth".
     simpl. apply bool_decide_unpack in Hfree.
-    iMod (ghost_map_insert l (Some v) with "Hauth") as "[Hauth Hpointsto]".
-    { replace l with (l +ₗ0) by apply Loc.add_0. by apply Hfree. }
+    iMod (ghost_map_insert_big (heap_array l (replicate n v)) with "Hauth") as "[Hauth Hpointsto]".
+    { apply heap_array_map_disjoint. destruct l as [l Hl]. intros i Hnz Hlt.
+      rewrite replicate_length in Hlt.
+      apply Hfree; first done. lia.
+    }
     iDestruct "Hauth" as "[Hauth Hauth']".
-    iFrame "Hinv". iFrame.
+    iFrame "Hinv".
+    replace (Z.to_nat n) with n by lia. iFrame.
     repeat iApply wpi_ret. iApply "Hwand". iModIntro. iMod "Hfupd".
     iMod ("Hclose" with "[Hauth]") as "_".
     { by iExists _. }
-    done.
+    iApply big_sep_map_list_heap_array. rewrite Loc.add_0 //.
+  Qed.
+
+  Lemma wpi_alloc_nondet M v Φ :
+    ↑heapH_inv_name ⊆ M →
+    (∀ l, l ↦ v -∗ Φ l) -∗
+    WPi alloc_nondet v @ H; M {{ Φ }}.
+  Proof.
+    iIntros (Hmask) "Hwand". iApply wpi_allocN_nondet; first done.
+    simpl. iIntros (l) "[Hpointsto _]". rewrite Loc.add_0. by iApply "Hwand".
   Qed.
 End wp_nondet.
